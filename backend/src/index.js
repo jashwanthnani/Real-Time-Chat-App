39e5d2df2acee88af6b383aee97ea185a3883cfe
Jashwanth Bavandlapalli <bavandlapallijashwanth@gmail.com>
2025-06-17 22:30:00 +0530

    added database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,17 @@
 import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import {connectDB} from "./lib/db.js";
+
+dotenv.config();
 const app = express();
 
-app.listen(5001, () => {
-    console.log("server is running on port 5001");
-})
+const PORT = process.env.PORT
+const MONGO_URI = process.env.MONGO_URI;
+
+app.use("/api/auth", authRoutes);    
+
+app.listen(PORT, () => {    
+    console.log("server is running on port PORT:" + PORT);
+    connectDB()
+})  
